f43c1c1ac9eeb8ba1a87d03e8fa636539cd1d786
Harshvardhan Singh <hvsingh46@gmail.com>
2025-12-09 00:32:09 +0530

    These are the codes I practiced today , i.e., 08/12/2025

--- GCD.c
+++ GCD.c
@@ -1,11 +1,11 @@
-// #include<stdio.h>
-// #include<math.h>
-// int main(){
-//     int m,n,a;
-//     printf("Enter the nums : ");
-//     scanf("%d %d",&m,&n);
-//     for(int i=1;i<=m && i<=n;i++){
-//         if(m%i==0 && n%i==0) a=i;
-//     }
-//     printf("The GCD is : %d",a);
-// }
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int m,n,a;
+    printf("Enter the nums : ");
+    scanf("%d %d",&m,&n);
+    for(int i=1;i<=m && i<=n;i++){
+        if(m%i==0 && n%i==0) a=i;
+    }
+    printf("The GCD is : %d",a);
+}
